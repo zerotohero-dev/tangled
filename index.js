6c228693a486169ce0e11957097ba1ba8e444166
Volkan Özçelik <volkan.ozcelik@gmail.com>
2017-05-04 04:40:17 +0000

    added fromJson

--- index.js
+++ index.js
@@ -1,24 +1,21 @@
 
-const slugify = function( str ) {
-    return ( '' + str )
-        .toLowerCase()
-        .replace( /\s+/g, '-' )
-        .replace( /[^\w\-]+/g, '' )
-        .replace( /\-\-+/g, '-' )
-        .replace( /^-+/g, '' )
-        .replace( /-+$/g, '' );
-}
+const fromJson = ( data ) => {
+    try {
+        return JSON.stringify( data );
+    } catch ( ignore ) {
+        void ignore;
 
+        return `${data}`;
+    }
+};
+
+const slugify = ( str ) => ( '' + str )
+    .toLowerCase()
+    .replace( /\s+/g, '-' )
+    .replace( /[^\w\-]+/g, '' )
+    .replace( /\-\-+/g, '-' )
+    .replace( /^-+/g, '' )
+    .replace( /-+$/g, '' );
+
+exports.fromJson = fromJson;
 exports.slugify = slugify;
-
-//const slugify = ( str ) => ( '' + str )
-//    .toLowerCase()
-//	    .replace( /\s+/g, '-' )
-//	        .replace( /[^\w\-]+/g, '' )
-//		    .replace( /\-\-+/g, '-' )
-//		        .replace( /^-+/g, '' )
-//			    .replace( /-+$/g, '' );
-//
-
-exports.slugify = slugify;
-
